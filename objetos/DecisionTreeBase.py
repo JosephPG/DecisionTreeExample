bdb4b4095a707eec113096db1247c34438e79a42
root <josehppg@gmail.com>
2018-08-05 21:26:54 +0000

    Add DecisionTreeBase object

---
+++ objetos/DecisionTreeBase.py
@@ -0,0 +1,32 @@
+from sklearn.tree import DecisionTreeClassifier
+
+
+class DecisionTreeBase(object):
+
+    def __init__(self, *args, **kwargs):
+        self._arbol = DecisionTreeClassifier()
+        self._feature_name = []
+        self._x_train = None
+        self._x_test = None
+        self._y_train = None
+        self._y_test = None
+
+    def _entrenar_arbol(self):
+        self.arbol.fit(self._x_train, self._y_train)
+
+    def _imp_score_predict(self):
+        """Porcentaje que asegura cuan seguro puede ser
+        acertar la prediccion"""
+        train_score_n = self._arbol.score(self._x_train, self._y_train)
+        test_score_n = self._arbol.score(self._x_test, self._y_test)
+        print("\n ======================")
+        print('Train Score: {}'.format(train_score_n))
+        print('Test Score: {}'.format(test_score_n))
+        print("====================== \n")
+
+    def _imp_predict(self):
+        """Imprime el resultado de la clasificacion"""
+        predict_result = self._arbol.predict(self._x_test)
+        print("\n ======================")
+        print('Prediccion: {}'.format(predict_result))
+        print("======================\n")
